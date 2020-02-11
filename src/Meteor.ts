60c7f24ea95bdf08287121f066f76ece2a950da2
Leonardo Venturini <leovenbag@gmail.com>
2020-02-11 14:40:04 -0400

    :seedling: meteor injection

---
+++ src/Meteor.ts
@@ -0,0 +1,23 @@
+type MessageCallback = (message: MeteorMessage) => void;
+
+export const injectOutboundInterceptor = (callback: MessageCallback) => {
+  const send = Meteor.connection._stream.send;
+
+  Meteor.connection._stream.send = function() {
+    send.apply(this, arguments);
+
+    callback({
+      content: arguments[0],
+      isOutbound: true,
+    });
+  };
+};
+
+export const injectInboundInterceptor = (callback: MessageCallback) => {
+  Meteor.connection._stream.on('message', function() {
+    callback({
+      content: arguments[0],
+      isInbound: true,
+    });
+  });
+};
