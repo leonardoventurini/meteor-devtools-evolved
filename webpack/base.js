9af2994903b2bc0ac535453f90f0918b814dcd1e
Leonardo Venturini <leovenbag@gmail.com>
2020-02-10 21:39:12 -0400

    :star: webpack config

--- webpack/base.js
+++ webpack/base.js
@@ -1,33 +1,59 @@
-var path = require('path');
-var _ = require('underscore');
+const path = require('path');
+const { extend } = require('lodash');
 
-const srcPath = path.join(__dirname, '../src/');
+const src = path.join(__dirname, '../src/');
 
-module.exports = function(overrides){
-  return _.extend({
-    entry: [
-      srcPath + 'main.jsx'
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.js/,
-          loaders: ['babel'],
-          include: srcPath
-        },
-        {
-          test: /\.css$/,
-          loader: 'style-loader!css-loader!postcss-loader'
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-        }
-      ]
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = override =>
+  extend(
+    {
+      entry: [path.resolve(src, 'App.jsx')],
+      plugins: [new CleanWebpackPlugin()],
+      module: {
+        rules: [
+          {
+            test: /\.js/,
+            loader: 'babel-loader',
+            include: src,
+          },
+          {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!postcss-loader',
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader'],
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/',
+                },
+              },
+            ],
+          },
+        ],
+      },
+      resolve: {
+        alias: {},
+        extensions: [
+          '.css',
+          '.eot',
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.sass',
+          '.scss',
+          '.ttf',
+          '.woff',
+        ],
+      },
     },
-    postcss: () => [require('precss')],
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-  }, overrides);
-};
+    override,
+  );
--- webpack/chrome.dev.js
+++ webpack/chrome.dev.js
@@ -1,21 +1,15 @@
-// This config is used when testing a local build 
-// of the extension in Chrome
+const base = require('./base');
+const path = require('path');
 
-var baseConfig = require('./base');
-var path = require('path');
-var webpack = require('webpack');
+module.exports = base({
+  watch: true,
 
-module.exports = baseConfig({
+  mode: 'development',
+
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '../chrome/build/')
+    path: path.join(__dirname, '../chrome/build/'),
   },
-  plugins : [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development'),
-      }
-    })
-  ],
-  devtool: 'inline-source-map'
+
+  devtool: 'inline-source-map',
 });
