067d27d7730ebc0d53011ad775d86c0663fdb4b2
Leonardo Venturini <leovenbag@gmail.com>
2020-02-12 18:44:20 -0400

    :wrench: fix source name

--- chrome/scripts/content.js
+++ chrome/scripts/content.js
@@ -1,19 +1,11 @@
 window.addEventListener('message', function(event) {
   // Only accept messages from same frame
-  if (event.source !== window) {
-    return;
-  }
+  if (event.source !== window) return;
 
-  var message = event.data;
+  const message = event.data;
 
   // Only accept messages that we know are ours
-  if (
-    typeof message !== 'object' ||
-    message === null ||
-    message.source !== 'ddp-monitor-extension'
-  ) {
-    return;
-  }
+  if (message.source !== 'meteor-devtools-evolved') return;
 
   chrome.runtime.sendMessage(message);
 });
