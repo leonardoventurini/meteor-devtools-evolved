1220032053ac90ce29692c1ead31646c720ee0b0
Leonardo Venturini <leovenbag@gmail.com>
2020-02-12 18:24:19 -0400

    :star: injector utils

--- src/Injector.ts
+++ src/Injector.ts
@@ -1,9 +1,49 @@
+import { extend } from 'lodash';
 import { injectInboundInterceptor, injectOutboundInterceptor } from './Meteor';
+import ErrorStackParser from 'error-stack-parser';
+import { EventTypes } from './EventTypes';
 
-injectInboundInterceptor((message: MeteorMessage) => {
-  console.log(message);
-});
+export const MESSAGE_SOURCE = 'meteor-devtools-evolved';
 
-injectOutboundInterceptor((message: MeteorMessage) => {
-  console.log(message);
-});
+export const sendMessage = (eventType: string, data: object) => {
+  window.postMessage(
+    {
+      eventType,
+      data,
+      source: MESSAGE_SOURCE,
+    },
+    '*',
+  );
+};
+
+export const getStackTrace = (stackTraceLimit: number) => {
+  const originalStackTraceLimit = Error.stackTraceLimit;
+
+  try {
+    Error.stackTraceLimit = stackTraceLimit ?? 15;
+    return ErrorStackParser.parse(new Error());
+  } finally {
+    Error.stackTraceLimit = originalStackTraceLimit;
+  }
+};
+
+export const sendDdpMessage = (message: MeteorMessage) => {
+  const stackTrace = getStackTrace(15);
+
+  if (stackTrace && stackTrace.length) {
+    stackTrace.splice(0, 2);
+  }
+
+  sendMessage(
+    EventTypes.DDP,
+    extend(message, {
+      trace: stackTrace,
+    }),
+  );
+};
+
+console.log('Meteor DevTools Evolved: Injecting script...');
+
+injectInboundInterceptor(sendDdpMessage);
+
+injectOutboundInterceptor(sendDdpMessage);
