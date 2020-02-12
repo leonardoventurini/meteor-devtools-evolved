c975ee5c27acb1ef72293c4ee07f4842550e001d
Leonardo Venturini <leovenbag@gmail.com>
2020-02-12 18:24:58 -0400

    :recycle: rearrange code

--- chrome/scripts/background.js
+++ chrome/scripts/background.js
@@ -1,21 +1,10 @@
-var connections = {};
-
-// Receive message from content script and relay to the devTools page for the
-// current tab
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // Messages from content scripts should have sender.tab set
-  if (sender.tab) {
-    var tabId = sender.tab.id;
-    if (tabId in connections) {
-      connections[tabId].postMessage(request);
-    }
-  }
-  return true;
-});
+const connections = {};
 
 chrome.runtime.onConnect.addListener(function(port) {
   // Listen to messages sent from the DevTools page
   port.onMessage.addListener(function(request) {
+    console.log(arguments);
+
     // Register initial connection
     if (request.name === 'init') {
       connections[request.tabId] = port;
@@ -31,4 +20,20 @@ chrome.tabs.onRemoved.addListener(function(tabId) {
   if (connections[tabId]) {
     delete connections[tabId];
   }
+});
+
+// Receive message from content script and relay to the devTools page for the
+// current tab
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(arguments);
+
+  // Messages from content scripts should have sender.tab set
+  if (sender.tab) {
+    const tabId = sender.tab.id;
+
+    if (tabId in connections) {
+      connections[tabId].postMessage(request);
+    }
+  }
+  return true;
 });
