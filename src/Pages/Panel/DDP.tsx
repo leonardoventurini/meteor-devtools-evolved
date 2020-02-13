de41a4d53531ca163bbc0436ab342b018382e9f3
Leonardo Venturini <leovenbag@gmail.com>
2020-02-13 14:57:58 -0400

    :star: optimize component

--- src/Pages/Panel/DDP.tsx
+++ src/Pages/Panel/DDP.tsx
@@ -1,10 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { PanelStoreConstructor } from '../../Stores/PanelStore';
+import { defer } from 'lodash';
 import { flow } from 'lodash/fp';
 import { inject, observer } from 'mobx-react';
-import { HTMLTable } from '@blueprintjs/core';
-import JSONTree from 'react-json-tree';
-import moment from 'moment';
+import { DDPMessage } from './DDP/DDPMessage';
 
 interface Props {
   panelStore?: PanelStoreConstructor;
@@ -14,57 +13,11 @@ export const DDP: FunctionComponent<Props> = flow(
   observer,
   inject('panelStore'),
 )(({ panelStore }) => {
-  const theme = {
-    scheme: 'google',
-    base00: 'transparent',
-    base01: '#282a2e',
-    base02: '#373b41',
-    base03: '#969896',
-    base04: '#b4b7b4',
-    base05: '#c5c8c6',
-    base06: '#e0e0e0',
-    base07: '#ffffff',
-    base08: '#CC342B',
-    base09: '#F96A38',
-    base0A: '#FBA922',
-    base0B: '#CED9E0',
-    base0C: '#eda794',
-    base0D: '#2B95D6',
-    base0E: '#A36AC7',
-    base0F: '#9ded9b',
-  };
-
-  const renderedMessages = panelStore?.ddp.map(message => (
-    <tr key={message.timestamp}>
-      <td>
-        {(message.isInbound && 'Inbound') || (message.isOutbound && 'Outbound')}
-      </td>
-      <td>
-        <JSONTree
-          data={JSON.parse(message.content)}
-          theme={theme}
-          invertTheme={false}
-          hideRoot
-        />
-      </td>
-      <td>
-        {message.timestamp && moment(message.timestamp).format('HH:mm:ss.SSS')}
-      </td>
-    </tr>
+  const logs = panelStore?.ddp.map(message => (
+    <DDPMessage message={message} key={message.timestamp} />
   ));
 
-  return (
-    <div>
-      <HTMLTable className='mde-table' condensed interactive>
-        <thead>
-          <tr>
-            <th>Flow</th>
-            <th>Content</th>
-            <th>Timestamp</th>
-          </tr>
-        </thead>
-        <tbody>{renderedMessages}</tbody>
-      </HTMLTable>
-    </div>
-  );
+  defer(() => window.scrollTo(0, document.body.scrollHeight));
+
+  return <div className='mde-ddp'>{logs}</div>;
 });
