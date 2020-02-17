9413363ba04ee2b46926f58df4b014b3cc32dfc1
Leonardo Venturini <leovenbag@gmail.com>
2020-02-17 19:31:22 -0400

    :rocket: use correct parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
-  parser: 'babel-eslint', // Specifies the ESLint parser
-  extends: [
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
