5abec522b53e32bb969a9f3ca8a44f1cf99c7e4f
Leonardo Venturini <leovenbag@gmail.com>
2020-02-17 19:34:01 -0400

    :recycle: simplify filter rendering

--- src/Pages/Panel/DDP/DDPFilterMenu.tsx
+++ src/Pages/Panel/DDP/DDPFilterMenu.tsx
@@ -2,52 +2,21 @@ import React, { FormEvent, FunctionComponent } from 'react';
 import { Switch } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import { usePanelStore } from '../../../Stores/PanelStore';
+import { FilterCriteria } from './FilterConstants';
 
 export const DDPFilterMenu: FunctionComponent = observer(() => {
   const store = usePanelStore();
 
-  return (
-    <div style={{ padding: 10 }}>
-      <Switch
-        checked={store.activeFilters.heartbeat}
-        label='Heartbeat'
-        onChange={(event: FormEvent<HTMLInputElement>) =>
-          store.setFilter('heartbeat', event.currentTarget.checked)
-        }
-      />
+  const filters = Object.keys(FilterCriteria).map(filter => (
+    <Switch
+      key={filter}
+      checked={store.activeFilters.heartbeat}
+      label={filter.charAt(0).toUpperCase() + filter.slice(1)}
+      onChange={(event: FormEvent<HTMLInputElement>) =>
+        store.setFilter(filter as FilterType, event.currentTarget.checked)
+      }
+    />
+  ));
 
-      <Switch
-        checked={store.activeFilters.subscription}
-        label='Subscription'
-        onChange={(event: FormEvent<HTMLInputElement>) =>
-          store.setFilter('subscription', event.currentTarget.checked)
-        }
-      />
-
-      <Switch
-        checked={store.activeFilters.collection}
-        label='Collection'
-        onChange={(event: FormEvent<HTMLInputElement>) =>
-          store.setFilter('collection', event.currentTarget.checked)
-        }
-      />
-
-      <Switch
-        checked={store.activeFilters.method}
-        label='Method'
-        onChange={(event: FormEvent<HTMLInputElement>) =>
-          store.setFilter('method', event.currentTarget.checked)
-        }
-      />
-
-      <Switch
-        checked={store.activeFilters.connection}
-        label='Connection'
-        onChange={(event: FormEvent<HTMLInputElement>) =>
-          store.setFilter('connection', event.currentTarget.checked)
-        }
-        style={{ marginBottom: 0 }}
-      />
-    </div>
-  );
+  return <div style={{ padding: 10 }}>{filters}</div>;
 });
