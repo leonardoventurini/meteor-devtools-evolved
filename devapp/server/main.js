c5d4d07bbdf6daec08a6710c7abbcf93726dee8d
Leonardo Venturini <leovenbag@gmail.com>
2020-02-17 19:45:37 -0400

    :recycle: devapp adjustments + formatting

--- devapp/server/main.js
+++ devapp/server/main.js
@@ -1,31 +1,21 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
+import { LinksCollection } from '../imports/api/links';
 
 function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
+  LinksCollection.insert({ title, url, createdAt: new Date() });
 }
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
+  if (LinksCollection.find().count() === 0) {
     insertLink(
       'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
+      'https://www.meteor.com/tutorials/react/creating-an-app',
     );
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
+    insertLink('Follow the Guide', 'http://guide.meteor.com');
 
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
+    insertLink('Read the Docs', 'https://docs.meteor.com');
 
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
+    insertLink('Discussions', 'https://forums.meteor.com');
   }
 });
