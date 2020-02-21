10b022114ce56a0f3905e44c6956a38d842c5efd
Leonardo Venturini <leovenbag@gmail.com>
2020-02-21 12:05:02 -0400

    :star: isolate status

--- src/Pages/Panel/DDP/DDP.tsx
+++ src/Pages/Panel/DDP/DDP.tsx
@@ -1,13 +1,11 @@
 import React, { FunctionComponent, useRef } from 'react';
-import { Button, Classes, Icon, Popover, Tag } from '@blueprintjs/core';
+import { Classes, Icon } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import { Hideable } from '../../../Utils/Hideable';
 import { usePanelStore } from '../../../Stores/PanelStore';
-import prettyBytes from 'pretty-bytes';
-import { DDPFilterMenu } from './DDPFilterMenu';
-import { Position } from '@blueprintjs/core/lib/esm/common/position';
 import { List } from 'react-virtualized';
 import { DDPRow } from './DDPRow';
+import { DDPStatus } from './DDPStatus';
 
 const Empty: FunctionComponent = () => (
   <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -50,53 +48,7 @@ export const DDP: FunctionComponent<Props> = observer(({ isVisible }) => {
         overscanRowCount={50}
       />
 
-      <div className='mde-layout__status'>
-        <div className='mde-layout__status__filter'>
-          <Popover content={<DDPFilterMenu />} position={Position.RIGHT_TOP}>
-            <Button icon='filter' text='Filter' />
-          </Popover>
-        </div>
-
-        {store.isLoading && (
-          <Tag minimal round style={{ marginRight: 10 }}>
-            Loading...
-          </Tag>
-        )}
-
-        <Tag minimal round style={{ marginRight: 10 }}>
-          <Icon
-            icon='cloud-download'
-            style={{ marginRight: 4, marginBottom: 1 }}
-            iconSize={12}
-          />
-          {prettyBytes(store.inboundBytes)}
-        </Tag>
-
-        <Tag minimal round style={{ marginRight: 10 }}>
-          <Icon
-            icon='cloud-upload'
-            style={{ marginRight: 4, marginBottom: 1 }}
-            iconSize={12}
-          />
-          {prettyBytes(store.outboundBytes)}
-        </Tag>
-
-        <Tag
-          intent='warning'
-          minimal
-          round
-          style={{ marginRight: 10 }}
-          interactive
-          onClick={() => store.clearLogs()}
-        >
-          <Icon
-            icon='inbox'
-            style={{ marginRight: 4, marginBottom: 1 }}
-            iconSize={12}
-          />
-          {store?.ddp.length}
-        </Tag>
-      </div>
+      <DDPStatus store={store} />
     </Hideable>
   );
 });
