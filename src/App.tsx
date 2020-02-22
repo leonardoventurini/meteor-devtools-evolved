f1dd53ed7b667e45c53f2bbbd87fe2763d6fca68
Leonardo Venturini <leovenbag@gmail.com>
2020-02-21 21:17:31 -0400

    :recycle: organize style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Panel } from './Pages/Panel';
 import { Options } from './Pages/Options';
 import { Popup } from './Pages/Popup';
 
-import './Style.scss';
+import './$App.scss';
 import { FocusStyleManager } from '@blueprintjs/core';
 
 FocusStyleManager.onlyShowFocusOnTabs();
