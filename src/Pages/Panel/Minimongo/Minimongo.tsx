6a2e1110175e0c69d4d3d7798b1d828744e4a403
Leonardo Venturini <leovenbag@gmail.com>
2020-02-26 17:52:26 -0400

    :star: collection selector

--- src/Pages/Panel/Minimongo/Minimongo.tsx
+++ src/Pages/Panel/Minimongo/Minimongo.tsx
@@ -1,35 +1,56 @@
-import React, { FunctionComponent } from 'react';
-import { Hideable } from '../../../Utils/Hideable';
+import { Button, Menu, MenuItem, Popover, Position } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
+import React, { FunctionComponent, useState } from 'react';
 import { usePanelStore } from '../../../Stores/PanelStore';
+import { Hideable } from '../../../Utils/Hideable';
 import { ObjectTree } from '../../../Utils/ObjectTree';
+import { StatusBar } from '../../Layout/StatusBar';
 
 interface Props {
   isVisible: boolean;
 }
 
 export const Minimongo: FunctionComponent<Props> = observer(({ isVisible }) => {
-  const panelStore = usePanelStore();
+  const { minimongoStore } = usePanelStore();
 
-  // const tree: ITreeNode[] = toPairs(panelStore.minimongoStore.collections).map(
-  //   ([key, documents]) => ({
-  //     id: key,
-  //     hasCaret: true,
-  //     icon: 'database',
-  //     label: key,
-  //     expanded: true,
-  //     childNodes: documents.map(document => ({
-  //       id: document._id,
-  //       hasCaret: false,
-  //       icon: 'document',
-  //       label: truncate(JSON.stringify(document)),
-  //     })),
-  //   }),
-  // );
+  const [activeCollection, setActiveCollection] = useState<null | string>(null);
+
+  const isActiveCollectionMissing =
+    activeCollection && !(activeCollection in minimongoStore.collections);
+
+  if (isActiveCollectionMissing) {
+    setActiveCollection(null);
+  }
 
   return (
     <Hideable isVisible={isVisible}>
-      <ObjectTree object={panelStore.minimongoStore.collections} />
+      <ObjectTree object={minimongoStore.collections} />
+
+      <StatusBar>
+        <div style={{ marginRight: 'auto' }}>
+          <Popover
+            content={
+              <Menu>
+                {Object.keys(minimongoStore.collections).map(key => (
+                  <MenuItem
+                    key={key}
+                    icon='database'
+                    text={key}
+                    active={activeCollection === key}
+                    onClick={() => setActiveCollection(key)}
+                  />
+                ))}
+              </Menu>
+            }
+            position={Position.RIGHT_TOP}
+          >
+            <Button
+              icon={activeCollection ? 'database' : null}
+              text={activeCollection || 'Select Collection'}
+            />
+          </Popover>
+        </div>
+      </StatusBar>
     </Hideable>
   );
 });
