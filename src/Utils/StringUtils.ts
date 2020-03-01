81afb75fc966ae013673f9129e943dcce3f48910
Leonardo Venturini <leovenbag@gmail.com>
2020-03-01 11:51:41 -0400

    :star: random color generator

--- src/Utils/StringUtils.ts
+++ src/Utils/StringUtils.ts
@@ -6,4 +6,25 @@ export namespace StringUtils {
       ? str.slice(0, max).concat('...')
       : str;
   };
+
+  /**
+   * Five levels of brightness from 1 to 5.
+   *
+   * @param brightness
+   */
+  export const getRandomColor = (brightness: number) => {
+    if (brightness < 1 || brightness > 5)
+      throw new Error(
+        'Only five brightness levels, from 1 to 5, are acceptable.',
+      );
+
+    const variance = 255 / 5;
+
+    const getByte = () =>
+      Math.round(variance * (brightness - 1) + Math.random() * variance);
+
+    const rgb = [0, 0, 0].map(getByte).join(',');
+
+    return `rgb(${rgb})`;
+  };
 }
