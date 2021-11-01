ef4ba204d393cfe2d6847efa06419a66b135874b
Leonardo Venturini <leovenbag@gmail.com>
2021-11-01 09:12:58 -0400

    :star: use @tstt/eslint-config and optimize packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,1 @@
-module.exports = {
-  semi: false,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-  bracketSpacing: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid"
-}
+module.exports = require('@tstt/eslint-config/.prettierrc.js')
