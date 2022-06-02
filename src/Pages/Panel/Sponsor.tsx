4d3629d2c6e8c03795d556cb2e1d863918560b9a
Leonardo Venturini <leovenbag@gmail.com>
2022-06-02 14:44:17 -0400

    :art: sponsor usability improvements

--- src/Pages/Panel/Sponsor.tsx
+++ src/Pages/Panel/Sponsor.tsx
@@ -3,6 +3,9 @@ import React, { FunctionComponent } from 'react'
 import { Separator } from '@/Components/Separator'
 import MeteorCloudOffer from '@/Assets/meteor-cloud-offer.png'
 
+import '@/Assets/meteor-shower.png'
+import { SponsorHero } from '@/Pages/Panel/Sponsor/SponsorHero'
+
 interface Props {
   isSponsorVisible: boolean
 
@@ -15,7 +18,7 @@ export const Sponsor: FunctionComponent<Props> = ({
 }) => {
   return (
     <Drawer
-      title='☁️ Deploy for Free'
+      title='☁️ Deploy for Free '
       isOpen={isSponsorVisible}
       onClose={onClose}
     >
@@ -25,77 +28,64 @@ export const Sponsor: FunctionComponent<Props> = ({
             src={MeteorCloudOffer}
             alt='Meteor Cloud Offer'
             style={{ width: '100%' }}
-            className='mb-4'
+            className='mb-4 cursor-pointer'
+            onClick={() => {
+              document
+                .querySelector('#meteor-cloud-hero')
+                .scrollIntoView({ behavior: 'smooth' })
+            }}
           />
-          <p>
-            ✅ Do you have a Meteor app, but you’re looking for a better hosting
-            platform?
-          </p>
 
-          <p>
-            ✅ Do you want to scale your Meteor app confidently with minimal
-            DevOps?
-          </p>
+          <div className='card w-full bg-base-100 shadow-xl mb-4'>
+            <div className='card-body'>
+              <p>
+                ✅ Do you have a Meteor app, but you’re looking for a better
+                hosting platform?
+              </p>
 
-          <p>
-            ✅ Do you want to save time and money by effectively reducing DevOps
-            expenses?
-          </p>
+              <p>
+                ✅ Do you want to scale your Meteor app confidently with minimal
+                DevOps?
+              </p>
 
-          <p>
-            If you answered yes to any of the questions above, we have the
-            ultimate solution for you!
-          </p>
+              <p>
+                ✅ Do you want to save time and money by effectively reducing
+                DevOps expenses?
+              </p>
 
-          <p>
-            Meteor Cloud provides a unique infrastructure designed for your
-            Meteor app. Some of Meteor Cloud’s well-loved features include:
-          </p>
+              <p>
+                If you answered yes to any of the questions above, we have the
+                ultimate solution for you!
+              </p>
 
-          <div className='mb-4'>
-            <div>💡 Free Automatic SSL</div>
-            <div>💡 Push to Deploy</div>
-            <div>💡 Outgoing IP Whitelist</div>
-            <div>💡 One command deploy</div>
-            <div>💡 Prerender SEO Optimization</div>
-            <div>💡 Rollback feature to revert changes</div>
-            <div>💡 Usage and warning notifications</div>
-            <div>💡 Meteor APM for performance monitoring</div>
-            <div>💡 Integration with a log system for issue monitoring</div>
-            <div>💡 Zero downtime with coordinated version updates</div>
-            <div>💡 Custom proxy layer tailored for Meteor applications</div>
-            <div>
-              💡 Triggers (Autoscaling) allow you to adapt to usage fluctuations
+              <p className='mb-4'>
+                Meteor Cloud provides a unique infrastructure designed for your
+                Meteor app. Some of Meteor Cloud’s well-loved features include:
+              </p>
+
+              <div>
+                <div>💡 Free Automatic SSL</div>
+                <div>💡 Push to Deploy</div>
+                <div>💡 Outgoing IP Whitelist</div>
+                <div>💡 One command deploy</div>
+                <div>💡 Prerender SEO Optimization</div>
+                <div>💡 Rollback feature to revert changes</div>
+                <div>💡 Usage and warning notifications</div>
+                <div>💡 Meteor APM for performance monitoring</div>
+                <div>💡 Integration with a log system for issue monitoring</div>
+                <div>💡 Zero downtime with coordinated version updates</div>
+                <div>
+                  💡 Custom proxy layer tailored for Meteor applications
+                </div>
+                <div>
+                  💡 Triggers (Autoscaling) allow you to adapt to usage
+                  fluctuations
+                </div>
+              </div>
             </div>
           </div>
 
-          <p>The best part? 👇</p>
-
-          <p>
-            As a Meteor DevTools Evolved user, you can use promo code{' '}
-            <span className='text-orange-500 font-bold'>DEVTOOLS60</span> to
-            receive a $60 credit towards any paid plan!
-          </p>
-
-          <p>
-            Sign up for a Meteor Cloud account{' '}
-            <a
-              href='https://social.meteor.com/devtools-evolved'
-              target='_blank'
-              rel='noreferrer'
-            >
-              here
-            </a>
-            , then email us at{' '}
-            <a
-              href='mailto:marketing@meteor.com'
-              target='_blank'
-              rel='noreferrer'
-            >
-              marketing@meteor.com
-            </a>{' '}
-            with your username and discount code.
-          </p>
+          <SponsorHero />
         </div>
       </div>
     </Drawer>
---
+++ src/Pages/Panel/Sponsor/SponsorHero.tsx
@@ -0,0 +1,89 @@
+import React, { FC } from 'react'
+import { StringUtils } from '@/Utils/StringUtils'
+import { AppToaster } from '@/AppToaster'
+
+interface Props {}
+
+export const SponsorHero: FC<Props> = () => {
+  return (
+    <div
+      className='hero'
+      style={{
+        height: '30rem',
+        backgroundImage: 'url(/build/assets/meteor-shower.png)',
+      }}
+      id='meteor-cloud-hero'
+    >
+      <div className='hero-overlay bg-opacity-70'></div>
+      <div className='hero-content text-center text-neutral-content'>
+        <div className='max-w-md'>
+          <h1 className='mb-5 text-5xl font-bold'>The best part? 👇</h1>
+          <p className='mb-5'>
+            As a Meteor DevTools Evolved user, you can use the promo code{' '}
+            <span
+              className='text-orange-500 font-bold cursor-pointer'
+              onClick={() => {
+                StringUtils.toClipboard('DEVTOOLS60')
+                AppToaster.show({
+                  icon: 'tick',
+                  message: 'Copied to Clipboard',
+                  intent: 'success',
+                  timeout: 1000,
+                })
+              }}
+            >
+              DEVTOOLS60
+            </span>{' '}
+            to receive a{' '}
+            <span className='text-green-500 font-bold'>$60 credit</span> towards
+            any paid plan!
+          </p>
+
+          <p>
+            Sign up for a Meteor Cloud account{' '}
+            <a
+              href='https://social.meteor.com/devtools-evolved'
+              target='_blank'
+              rel='noreferrer'
+              className='link link-accent'
+            >
+              here
+            </a>
+            , then email us at{' '}
+            <a
+              href='mailto:marketing@meteor.com'
+              target='_blank'
+              rel='noreferrer'
+              className='link link-accent'
+            >
+              marketing@meteor.com
+            </a>{' '}
+            with your username and discount code.
+          </p>
+
+          <div className='flex gap-2 justify-center'>
+            <button
+              className='btn btn-primary'
+              onClick={() =>
+                chrome.tabs.create({
+                  url: 'https://social.meteor.com/devtools-evolved',
+                })
+              }
+            >
+              Sign Up
+            </button>
+
+            <button
+              className='btn bg-orange-500 hover:bg-orange-600'
+              onClick={() =>
+                chrome.tabs.create({ url: 'mailto:marketing@meteor.com' })
+              }
+            >
+              Email Us the Code
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
