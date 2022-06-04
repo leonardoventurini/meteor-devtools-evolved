da2cdd35b16e4667ab776402d976e8a6c768acb7
Leonardo Venturini <leovenbag@gmail.com>
2022-06-04 10:24:24 -0400

    :art: remove card

--- src/Pages/Panel/Sponsor.tsx
+++ src/Pages/Panel/Sponsor.tsx
@@ -30,51 +30,47 @@ export const Sponsor: FunctionComponent<Props> = ({
       <div className={Classes.DRAWER_BODY}>
         <SponsorHero />
         <div className={Classes.DIALOG_BODY}>
-          <div className='card w-full bg-base-100 shadow-xl mb-4 text-lg'>
-            <div className='card-body'>
-              <p>
-                ✅ Do you have a Meteor app, but you’re looking for a better
-                hosting platform?
-              </p>
+          <div className='w-full mb-4 text-lg'>
+            <p>
+              ✅ Do you have a Meteor app, but you’re looking for a better
+              hosting platform?
+            </p>
 
-              <p>
-                ✅ Do you want to scale your Meteor app confidently with minimal
-                DevOps?
-              </p>
+            <p>
+              ✅ Do you want to scale your Meteor app confidently with minimal
+              DevOps?
+            </p>
 
-              <p>
-                ✅ Do you want to save time and money by effectively reducing
-                DevOps expenses?
-              </p>
+            <p>
+              ✅ Do you want to save time and money by effectively reducing
+              DevOps expenses?
+            </p>
 
-              <p>
-                If you answered yes to any of the questions above, we have the
-                ultimate solution for you!
-              </p>
+            <p>
+              If you answered yes to any of the questions above, we have the
+              ultimate solution for you!
+            </p>
 
-              <p className='mb-4'>
-                Meteor Cloud provides a unique infrastructure designed for your
-                Meteor app. Some of Meteor Cloud’s well-loved features include:
-              </p>
+            <p className='mb-4'>
+              Meteor Cloud provides a unique infrastructure designed for your
+              Meteor app. Some of Meteor Cloud’s well-loved features include:
+            </p>
 
+            <div>
+              <div>💡 Free Automatic SSL</div>
+              <div>💡 Push to Deploy</div>
+              <div>💡 Outgoing IP Whitelist</div>
+              <div>💡 One command deploy</div>
+              <div>💡 Prerender SEO Optimization</div>
+              <div>💡 Rollback feature to revert changes</div>
+              <div>💡 Usage and warning notifications</div>
+              <div>💡 Meteor APM for performance monitoring</div>
+              <div>💡 Integration with a log system for issue monitoring</div>
+              <div>💡 Zero downtime with coordinated version updates</div>
+              <div>💡 Custom proxy layer tailored for Meteor applications</div>
               <div>
-                <div>💡 Free Automatic SSL</div>
-                <div>💡 Push to Deploy</div>
-                <div>💡 Outgoing IP Whitelist</div>
-                <div>💡 One command deploy</div>
-                <div>💡 Prerender SEO Optimization</div>
-                <div>💡 Rollback feature to revert changes</div>
-                <div>💡 Usage and warning notifications</div>
-                <div>💡 Meteor APM for performance monitoring</div>
-                <div>💡 Integration with a log system for issue monitoring</div>
-                <div>💡 Zero downtime with coordinated version updates</div>
-                <div>
-                  💡 Custom proxy layer tailored for Meteor applications
-                </div>
-                <div>
-                  💡 Triggers (Autoscaling) allow you to adapt to usage
-                  fluctuations
-                </div>
+                💡 Triggers (Autoscaling) allow you to adapt to usage
+                fluctuations
               </div>
             </div>
           </div>
