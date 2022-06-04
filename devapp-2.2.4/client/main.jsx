7b4fcdeb7098849725d51278d05c300eb44214b0
Leonardo Venturini <leovenbag@gmail.com>
2022-06-04 10:06:36 -0400

    :wrench: copy project files

--- devapp-2.2.4/client/main.jsx
+++ devapp-2.2.4/client/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+import React from 'react'
+import { Meteor } from 'meteor/meteor'
+import { render } from 'react-dom'
+import { App } from '../imports/ui/App'
+
+import '../imports/api/links'
+import '../imports/api/random'
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
-});
+  render(<App />, document.getElementById('react-target'))
+})
---
+++ devapp-2.2.4/imports/api/random.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo'
+
+export const RandomCollection = new Mongo.Collection('random')
