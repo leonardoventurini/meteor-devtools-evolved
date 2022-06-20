e508ef87f2f353ba71dc167f82236b58722323a3
niloy <nilooy.dev@gmail.com>
2022-06-21 00:23:45 +0200

    :zap: Add husky for pre-commit lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,27 @@
 const { merge } = require('lodash')
 
 module.exports = merge(require('@tstt/eslint-config/index.js'), {
-  globals: { Meteor: 'readonly', i18n: 'readonly' },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint'],
+    rules: {
+        'global-require': 0,
+        '@typescript-eslint/no-var-requires': 0,
+    },
+    globals: { Meteor: 'readonly', i18n: 'readonly' },
 })
