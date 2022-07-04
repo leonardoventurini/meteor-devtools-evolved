3c64e3c2f19e5250391a7b93d890810dd792967a
Leonardo Venturini <leovenbag@gmail.com>
2022-07-04 14:07:55 -0400

    :wrench: fix woff and ttf js

--- webpack/base.js
+++ webpack/base.js
@@ -87,18 +87,6 @@ module.exports = (browser = 'chrome', override) => {
             use: ['style-loader', 'css-loader', 'sass-loader'],
           },
           {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/',
-                },
-              },
-            ],
-          },
-          {
             test: /\.(gif|png|jpg)$/,
             use: [
               {
