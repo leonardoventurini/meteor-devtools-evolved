2932f802702dcf10ed4bcfacb2882d594b9c2e3b
Leonardo Venturini <leovenbag@gmail.com>
2022-12-05 10:54:12 -0400

    :wrench: cleanup non-plain objects into strings with constructor name

--- src/Injectors/MinimongoInjector.ts
+++ src/Injectors/MinimongoInjector.ts
@@ -2,13 +2,44 @@ import { warning } from '@/Log'
 import { Registry, sendMessage } from '@/Browser/Inject'
 import cloneDeep from 'lodash/cloneDeep'
 import throttle from 'lodash/throttle'
+import { isArray } from 'lodash'
 
 const cleanup = (object: any) => {
+  if (typeof object !== 'object') return object
+
   const clonedObject = cloneDeep(object)
 
   Object.keys(clonedObject).forEach((key: string) => {
-    if (clonedObject[key] instanceof Date) {
-      clonedObject[key] = clonedObject[key].toString()
+    if (!clonedObject[key]) {
+      return
+    }
+
+    if (typeof clonedObject[key] === 'object') {
+      if (isArray(clonedObject[key])) {
+        clonedObject[key] = clonedObject[key].map((item: any) => cleanup(item))
+        return
+      }
+
+      if (clonedObject[key] instanceof Date) {
+        clonedObject[key] = `[Object::${
+          clonedObject[key].constructor.name
+        }] ${clonedObject[key].toISOString()}`
+        return
+      }
+
+      if (clonedObject[key].constructor.name !== 'Object') {
+        if (typeof clonedObject[key].toString === 'function') {
+          clonedObject[key] = `[Object::${
+            clonedObject[key].constructor.name
+          }] ${clonedObject[key].toString()}`
+          return
+        } else {
+          clonedObject[key] = `[Object::${clonedObject[key].constructor.name}]`
+          return
+        }
+      }
+
+      clonedObject[key] = cleanup(clonedObject[key])
     }
   })
 
