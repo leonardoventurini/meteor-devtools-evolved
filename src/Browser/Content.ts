e87710c20c8c65ff2a2d68f9aff7f54e3dbdb706
Nikolay <tehlic@gmail.com>
2026-05-01 13:56:11 +0400

    fix globalThis to make TS happy

--- src/Browser/Content.ts
+++ src/Browser/Content.ts
@@ -2,18 +2,18 @@ import browser from 'webextension-polyfill'
 
 const messageHandler = (event: MessageEvent) => {
   // Only accept messages from same frame
-  if (event.source !== globalThis) return
+  if (event.source !== (globalThis as unknown as Window)) return
 
   // Only accept messages that we know are ours
   if (event.data.source !== 'meteor-devtools-evolved') return
 
   browser.runtime.sendMessage(event.data).catch(() => {
     // Cleans up and prevent "context invalidated" errors.
-    window.removeEventListener('message', messageHandler)
+    globalThis.removeEventListener('message', messageHandler)
   })
 }
 
-window.addEventListener('message', messageHandler)
+globalThis.addEventListener('message', messageHandler)
 
 const url = browser.runtime.getURL('/dist/inject.js')
 const script = document.createElement('script')
