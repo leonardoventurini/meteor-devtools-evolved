a0810e72b2c2cab3a0458160468c9dcfae020de1
Leonardo Venturini <leovenbag@gmail.com>
2026-09-03 15:55:08 -0400

    fix: defer Meteor 2 secondary connection

--- devapp-2.16/client/additional-connection.js
+++ devapp-2.16/client/additional-connection.js
@@ -3,19 +3,27 @@ import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { FIXTURE_COLLECTION_NAMES } from '../imports/api/fixture-data'
 
-export const additionalConnection = DDP.connect(Meteor.absoluteUrl())
-export const FixtureRemote = new Mongo.Collection(
-  FIXTURE_COLLECTION_NAMES.remote,
-  { connection: additionalConnection },
-)
+export let additionalConnection
+export let FixtureRemote
 
 export const remoteReady = new Promise((resolve, reject) => {
-  additionalConnection.subscribe('fixture.remote', {
-    onReady() {
-      resolve({ ready: true })
-    },
-    onError(error) {
-      reject(error)
-    },
+  Meteor.startup(() => {
+    // Let document-start extension instrumentation install before opening the
+    // additional socket, so this connection is observable independently.
+    Meteor.defer(() => {
+      additionalConnection = DDP.connect(Meteor.absoluteUrl())
+      FixtureRemote = new Mongo.Collection(FIXTURE_COLLECTION_NAMES.remote, {
+        connection: additionalConnection,
+      })
+
+      additionalConnection.subscribe('fixture.remote', {
+        onReady() {
+          resolve({ ready: true })
+        },
+        onError(error) {
+          reject(error)
+        },
+      })
+    })
   })
 })
